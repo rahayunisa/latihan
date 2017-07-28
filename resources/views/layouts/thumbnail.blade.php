b25c2601b1c0a51d4ea2df00f2770e462d217d22
unknown <151610208@smkassalaambandung.sch.id>
2017-07-28 16:24:40 +0700

    Belajar Menggunakan Template

--- resources/views/layouts/thumbnail.blade.php
+++ resources/views/layouts/thumbnail.blade.php
@@ -23,30 +23,36 @@
 						<img alt="Bootstrap Thumbnail Second" src="http://lorempixel.com/output/city-q-c-600-200-1.jpg">
 						<div class="caption">
 							<h3>
-								Thumbnail label
+								Misteri Kereta Kencana yang Lewat Tiap Malam Jumat di Ngaliyan
 							</h3>
 							<p>
-								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
-							</p>
-							<p>
-								<a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
-							</p>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-4">
-					<div class="thumbnail">
-						<img alt="Bootstrap Thumbnail Third" src="http://lorempixel.com/output/sports-q-c-600-200-1.jpg">
-						<div class="caption">
-							<h3>
-								Thumbnail label
-							</h3>
-							<p>
-								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
-							</p>
-							<p>
-								<a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
-							</p>
+								Jam baru menunjukkan setengah sebelas malam, sementara hujan gerimis membasahi malam Jumat sejak Magrib. Ngaliyan malam itu begitu dingin dan senyap. Penghuninya sudah terlelap.
+
+Tapi entah kenapa Parmin susah memejamkan mata. Diambilnya rokok, berharap dapat hawa melawan dingin. Parmin duduk di ruang tamunya yang temaram.
+
+Dihisap dalam-dalam batang rokok pertamanya. Detak jam dinding mengiringi kepulan asap dari mulut Parmin. Sesekali Parmin melongok ke jendela kaca, menyibak sedikit tirainya. Masih gerimis dan sepi, tak ada seorangpun lewat.
+
+Batang rokok kedua diambilnya. Parmin kembali menyalakan rokok dan menghisapnya. Kembali pandangannya tertuju ke kaca jendela. Kali ini Parmin mendapati cahaya yang berjalan dari ujung gang.
+
+Kebetulan rumah Parmin pada posisi tusuk sate, tepat di depan gang. Kakek Parmin yang mewariskan rumah itu berpesan agar setiap malam Jumat menaruh sebutir telur dan kembang setaman di depan pintu. Menurut sang kakek, rumah tusuk sate kerap menjadi jujugan lelembut.
+
+Cahaya kian mendekat, ke arah rumah Parmin. Tapi kepulan asap rokok menghalangi pandangan Parmin. Ia kibaskan tangannya mengusir asap. Pandangannya dari balik kaca jendela sedikit lebih jelas, sekaligus membuat jantungnya berdegub.
+
+Dilihatnya sepasang kuda putih menarik kereta, diatasnya duduk perempuan berbaju putih, rambutnya panjang menjuntai. Memakai mahkota.
+Darah Parmin berdesir, keningnya mulai berkeringat. Pikir Parmin, tak mungkin kalau itu orang. Apalagi tetangganya. “Masak iya ada yang punya kuda? Ini pasti lelembut yang dimaksud Mbah Kakung,” batinnya.
+Degub jantung Parmin kian kencang saat didengar suara gemerincing dan cahaya makin dekat.
+
+Parmin mulai bimbang, tetap menatap lelembut itu hingga mengambil sesajen yang sore tadi diletakkannya di depan pintu. Atau masuk saja ke kamar.
+Tapi akhirnya Parmin memutuskan meladeni penasarannya. “Seperti apa rupa lelembut yang diceritakan Mbah Kakung? Apa seperti ratu laut selatan yang ada di film? Mungkin memang itu, ya benar,” batin Parmin mengoceh, tapi ia gemetaran.
+
+Suara gemerincing semakin jelas, cahaya kian mendekat, tapi sayang gerimis menjadi hujan agak lebat. Pandangan Parmin kian terhalang. Tapi rasa penasarannya kian membuncah. Akhirnya Parmin memberanikan diri beranjak dari duduknya di dekat jendela dan membuka pintu.
+“Krincing .. krincing .. krincing..” makin jelas. Pintu terbuka, hujan bertambah lebat. “Krincing..krincing,” … Parmin tertegun.
+
+“Sate mas? Sekalian saya numpang berteduh di teras ya mas” kata lelaki yang sedari tadi mendorong gerobak satenya dari ujung gang.
+Tukang sate menutup payung yang lekat di gerobak dan membuka jas hujan biru mudanya.
+
+“Asuuuuuuuuuuuuuuuuu!!!” teriak Parmin dalam hati, sambil menyeka keningnya yang basah. “Monggo mas, saya mau tidur,” Parmin menguap menuju kamar.
+							
 						</div>
 					</div>
 				</div>
